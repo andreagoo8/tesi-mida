7b69a1854b9eb65f2918245087d79022b594ebc5
Andrea Naclerio <andrea.naclerio@studenti.unitn.it>
2024-06-30 04:30:58 +0200

    Some improvements...

--- plots/mpl/test.py
+++
@@ -1,9 +0,0 @@
-alga = 'chlorella'
-content = 'ashes'
-
-subfig_title = ' {content_cap} {alga}_{content} '
-print(subfig_title.format(alga = alga, content = content, content_cap = content.capitalize()) + '\n')
-
-
-# print(formatted_filename)
-# print()
